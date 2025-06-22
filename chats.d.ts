68d10426667e926bd9979a31710883e73e171949
kingofhaze <194982118+kingofhaze@users.noreply.github.com>
2025-06-21 22:33:16 -0400

    C15L6

---
+++ chats.d.ts
@@ -0,0 +1,10 @@
+export declare module "./chats.js" {
+  export type Chat = {
+    time: string;
+    message: string;
+  };
+
+  export function log(chats: Chat[]): void;
+
+  export const chats: Chat[];
+}
